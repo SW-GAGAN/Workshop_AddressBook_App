9c62fb88597370e7d727e15687605cae56c241bd
SW-GAGAN <srgaganreddy0@gmail.com>
2021-09-06 20:03:02 +0530

    Added: Submit button fuunctionality create JSON object

--- js/AddressBookData.js
+++ js/AddressBookData.js
@@ -1,58 +1,58 @@
 // UC-4
-class AddressBookData{
+class AddressBookData {
 
-    get fullname(){ return this._fullname; }
-    set fullname(fullname){
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-        if( nameRegex.test(fullname)){
-            this._fullname=fullname;
+    get fullname() { return this._fullname; }
+    set fullname(fullname) {
+        let nameRegex = RegExp('^[A-Z_]{1}[a-zA-Z_ ]{3,}$');
+        if (nameRegex.test(fullname)) {
+            this._fullname = fullname;
         }
-        else throw 'Invalid Name';  
+        else throw 'Invalid Name';
     }
 
-    get phone(){ return this._phone; }
-    set phone(phone){
+    get phone() { return this._phone; }
+    set phone(phone) {
         let phoneRegex1 = RegExp(/^[\+]?[(]?[0-9]{3}[)]?[0-9]{3}[-\s\.]?[0-9]{6,13}$/im);
         let phoneRegex2 = RegExp(/^[0-9]{3}[0-9]{7,11}$/im);
-        if( phoneRegex1.test(phone)){
-            this._phone=phone;
+        if (phoneRegex1.test(phone)) {
+            this._phone = phone;
         }
-        else if( phoneRegex2.test(phone)){
-            this._phone=phone;
+        else if (phoneRegex2.test(phone)) {
+            this._phone = phone;
         }
-        else throw 'Invalid Phone Number';  
+        else throw 'Invalid Phone Number';
     }
 
-    get address(){ return this._address; }
-    set address(address){
-        let addressRegex = RegExp('[A-Z]{1}[a-z]{3,}');
-        if( addressRegex.test(address)){
-            this._address=address;
+    get address() { return this._address; }
+    set address(address) {
+        let addressRegex = RegExp('^[a-zA-Z0-9_][a-zA-Z0-9_ ]{4,}$');
+        if (addressRegex.test(address)) {
+            this._address = address;
         }
-        else throw 'Invalid Address';  
-    }
-    
-    get city(){ return this._city; }
-    set city(city){
-        this._city=city;
-    }
-    
-    get state(){ return this._state; }
-    set state(state){
-        this._state=state;
+        else throw 'Invalid Address';
     }
 
-    get zip(){ return this._zip; }
-    set zip(zip){
+    get city() { return this._city; }
+    set city(city) {
+        this._city = city;
+    }
+
+    get state() { return this._state; }
+    set state(state) {
+        this._state = state;
+    }
+
+    get zip() { return this._zip; }
+    set zip(zip) {
         let zipRegex = RegExp('^[1-9]{1}[0-9]{2}[\\s]?[0-9]{3}$');
-        if( zipRegex.test(zip)){
-            this._zip=zip;
+        if (zipRegex.test(zip)) {
+            this._zip = zip;
         }
-        else throw 'Invalid Zip-Code';  
+        else throw 'Invalid Zip-Code';
     }
-    
-    toString(){
-        return "fullname = '"+this.fullname + ", phone = '" + this.phone + ", address ='" + this.address +
-                        ", city = '"+this.city+ ", state = '" + this.state +", zip-code = '" + this.zip; 
+
+    toString() {
+        return "fullname = '" + this.fullname + ", phone = '" + this.phone + ", address ='" + this.address +
+            ", city = '" + this.city + ", state = '" + this.state + ", zip-code = '" + this.zip;
     }
 }
